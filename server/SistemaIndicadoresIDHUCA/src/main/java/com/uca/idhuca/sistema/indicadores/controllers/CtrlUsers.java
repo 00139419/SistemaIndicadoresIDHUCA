e33bc52ce2a9d96797d5c8490ef4c273a5ae3223
Andrea-09 <00366519@uca.edu.sv>
2025-04-27 21:45:52 -0600

    Server: adding add, update and delete methods to CtrlUsers

--- server/SistemaIndicadoresIDHUCA/src/main/java/com/uca/idhuca/sistema/indicadores/controllers/CtrlUsers.java
+++ server/SistemaIndicadoresIDHUCA/src/main/java/com/uca/idhuca/sistema/indicadores/controllers/CtrlUsers.java
@@ -19,11 +19,14 @@ import com.uca.idhuca.sistema.indicadores.repositories.IRepoUsuario;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
 @Slf4j
 @RestController
 @RequestMapping(ROOT_CONTEXT + "users")
 public class CtrlUsers {
-	
+
 	@Autowired
 	IRepoUsuario repoUsuario;
 
@@ -32,23 +35,92 @@ public class CtrlUsers {
 		GenericEntityResponse<List<Usuario>> response = null;
 		String key = "SYSTEM";
 		log.info("[" + key + "] ------ Inicio de servicio 'users/get/all'");
-		
+
 		try {
 			List<Usuario> list = repoUsuario.findAll();
 			response = new GenericEntityResponse<>();
 			response.setCodigo(OK);
 			response.setMensaje("Datos obtenidos correctamente");
 			response.setEntity(list);
-				
+
 			log.info("repoUsuario" + repoUsuario);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(response != null) {
+			if (response != null) {
 				log.info("[" + key + "] ------ Fin de servicio 'users/get/all' " + response.toJson());
 			}
 		}
-		return new ResponseEntity<GenericEntityResponse<List<Usuario>>>(response, HttpStatus.OK);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<GenericEntityResponse<Usuario>> addUser(@RequestBody Usuario usuario) {
+		GenericEntityResponse<Usuario> response = new GenericEntityResponse<>();
+		try {
+			Usuario savedUser = repoUsuario.save(usuario);
+			response.setCodigo(OK);
+			response.setMensaje("Usuario agregado correctamente");
+			response.setEntity(savedUser);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setCodigo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setMensaje("Error al agregar usuario");
+		}
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/delete/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<GenericEntityResponse<Void>> deleteUser(@PathVariable Long id, @RequestBody Usuario usuario) {
+		GenericEntityResponse<Void> response = new GenericEntityResponse<>();
+		try {
+			if (repoUsuario.existsById(id)) {
+				// Ensure the ID in the path matches the ID in the request body
+				if (!id.equals(usuario.getId())) {
+					response.setCodigo(HttpStatus.BAD_REQUEST.value());
+					response.setMensaje("El ID en la URL no coincide con el ID en el cuerpo de la solicitud");
+					return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+				}
+				repoUsuario.deleteById(id);
+				response.setCodigo(OK);
+				response.setMensaje("Usuario eliminado correctamente");
+			} else {
+				response.setCodigo(HttpStatus.NOT_FOUND.value());
+				response.setMensaje("Usuario no encontrado");
+				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setCodigo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setMensaje("Error al eliminar usuario");
+		}
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@PutMapping(value = "/update/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<GenericEntityResponse<Usuario>> updateUser(@PathVariable Long id, @RequestBody Usuario usuario) {
+		GenericEntityResponse<Usuario> response = new GenericEntityResponse<>();
+		try {
+			if (repoUsuario.existsById(id)) {
+				if (!id.equals(usuario.getId())) {
+					response.setCodigo(HttpStatus.BAD_REQUEST.value());
+					response.setMensaje("El ID en la URL no coincide con el ID en el cuerpo de la solicitud");
+					return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+				}
+				Usuario updatedUser = repoUsuario.save(usuario);
+				response.setCodigo(OK);
+				response.setMensaje("Usuario actualizado correctamente");
+				response.setEntity(updatedUser);
+			} else {
+				response.setCodigo(HttpStatus.NOT_FOUND.value());
+				response.setMensaje("Usuario no encontrado");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setCodigo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setMensaje("Error al actualizar usuario");
+		}
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
